070f877a18c50a102ccda8a5bd5f65a5d9320099
Dustin Chabrowski <dustin.duske@gmail.com>
2016-02-17 19:45:17 +0100

    [TASK] add rollup plugin to resolve relative paths

--- app/scripts/derp.js
+++ app/scripts/derp.js
@@ -1,6 +1,6 @@
 export function dateClicker(elementId) {
   document.getElementById(elementId).addEventListener('click', (event) => {
-    console.log('clicked!');
+    console.log(`clicked on ${event.target}`);
     event.target.text = new Date();
   });
 }
